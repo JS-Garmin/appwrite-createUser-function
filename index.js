d6d07748a7ae1e47b115669d33c5608491c00dfc
JS-Garmin <johannesschwind@gmail.com>
2026-02-26 13:14:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const sdk = require('node-appwrite');
 
 module.exports = async ({ req, res, log, error }) => {
   const client = new sdk.Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_ENDPOINT || 'https://fra.cloud.appwrite.io/v1')
+    .setEndpoint('https://fra.cloud.appwrite.io/v1') 
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(process.env.APPWRITE_FUNCTION_API_KEY);
 
   const users = new sdk.Users(client);
 
   try {
-    // Nur users.list() aufrufen, keine Parameter übergeben!
+    log("Abrufen gestartet...");
     const response = await users.list();
     return res.json(response);
   } catch (err) {
-    error("SDK Error: " + err.message);
+    error("Fehler: " + err.message);
     return res.json({ error: err.message }, 500);
   }
 };
